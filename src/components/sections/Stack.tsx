56ca9426740e305c7bb04522d33f322150ee03a4
Alexis Delvalle <160870111+Ale-Delvalle@users.noreply.github.com>
2026-04-29 15:31:29 -0300

    feat: stack row

--- src/components/sections/Stack.tsx
+++ src/components/sections/Stack.tsx
@@ -3,7 +3,17 @@ import styles from "./Stack.module.css";
 export function Stack() {
     return (
         <section className={styles.container}>
-            <h2>Stack</h2>
+            <h1>Stack</h1>
+            <section className={styles.stackRow}>
+                <p>TypeScript</p>
+                <p>NodeJS</p>
+                <p>NestJS</p>
+                <p>PostgreSQL</p>
+                <p>MongoDB</p>
+                <p>Prisma</p>
+                <p>TailwindCSS</p>
+                <p>Git</p>
+            </section>
         </section>
     );
 }

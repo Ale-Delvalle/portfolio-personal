8bd80f47844beda10c687924c337a31b60a2f76f
Alexis Delvalle <160870111+Ale-Delvalle@users.noreply.github.com>
2026-04-29 19:25:58 -0300

    style: fondo color salmón.

--- src/components/sections/Stack.tsx
+++ src/components/sections/Stack.tsx
@@ -3,16 +3,24 @@ import styles from "./Stack.module.css";
 export function Stack() {
     return (
         <section className={styles.container}>
-            <h1>Stack</h1>
+            <h1 className={styles.title}>Stack</h1>
             <section className={styles.stackRow}>
                 <p>TypeScript</p>
                 <p>NodeJS</p>
                 <p>NestJS</p>
                 <p>PostgreSQL</p>
                 <p>MongoDB</p>
-                <p>Prisma</p>
-                <p>TailwindCSS</p>
+                <p>React</p>
+                <p>React Native</p>
+                <p>CSS</p>
                 <p>Git</p>
+            </section>
+            <h2 className={styles.secondaryTitle}>Tambien he trabajado en algunos proyectos puntuales con:</h2>
+            <section className={styles.secondaryStack}>
+                <p>Electrón</p>
+                <p>Google APIs</p>
+                <p>Express</p>
+                <p>Tailwind</p>
             </section>
         </section>
     );

19860f6de22e0c27596c10a2b5cc8958cc9852c2
Alexis Delvalle <160870111+Ale-Delvalle@users.noreply.github.com>
2026-05-17 20:54:38 -0300

    Versiones alternativas de secciones de la pagina han sido borradas.

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,7 @@ import { Navbar } from './components/layout/Navbar';
 import { GlowBackground } from './components/layout/GlowBackground';
 import { Hero } from './components/sections/Hero';
 import { Stack } from './components/sections/Stack';
-import { Projects } from './components/sections/Projects';
-import { HeroImproved } from './components/sections/HeroImproved';
-import { HeroV3 } from './components/sections/HeroV3';
+// import { Projects } from './components/sections/Projects';
 // import { ProjectsV1 } from './components/sections/ProjectsV1';
 import { ProjectsV2 } from './components/sections/ProjectsV2';
 
@@ -19,9 +17,7 @@ function App() {
       <Navbar theme={theme} toggleTheme={toggleTheme} />
       <Hero />
       <Stack />
-      <Projects />
-      <HeroImproved />
-      <HeroV3 />
+      {/* <Projects /> */}
       {/* <ProjectsV1 /> */}
       <ProjectsV2 />
     </>
